2c7f7c5af30c13b51d20798aa15a8231b38d73e2
Nikita Osovskiy <nikitaosovskiy2005ok181@gmail.com>
2024-04-12 22:23:30 +0300

    bot modification

--- bot.py
+++ bot.py
@@ -1,38 +1,21 @@
-#!venv/bin/python
-# -*- encoding: utf-8 -*-
+# python3.11
 import logging
+import asyncio
 
 import geopy.distance as geo
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.utils.executor import start_webhook
+from aiogram import Bot, Dispatcher, types, Router
+from aiogram.enums import ParseMode
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.filters import Command, CommandStart
 
 import config
 
-WEBHOOK_HOST = 'https://pmpu.site'
-WEBHOOK_PATH = '/lecTop/'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-WEBAPP_HOST = '127.0.0.1'
-WEBAPP_PORT = 7787
+form_router = Router()
 
-bot = Bot(token=config.TOKEN)
-
-storage = MemoryStorage()
-# Диспетчер для бота
-dp = Dispatcher(bot, storage=storage)
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
-
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-
-async def on_shutdown(dp):
-    await bot.delete_webhook()
 
 
 # Zocollo
@@ -46,122 +29,125 @@ org_loc5 = 59.881671
 org_loc6 = 29.830563
 result = [0, 0, 0]
 
-org_loc = (org_loc1, org_loc2)
+org_loc = (org_loc3, org_loc4)
+
+bands = [
+        "Группа 1",
+        "Группа 2",
+        "Группа 3",
+        ]
+
+result = {
+        "Лучший вокал": [0]*len(bands),
+        "Лучший инструментал": [0]*len(bands),
+        "Лучшее выступление": [0]*len(bands),
+        }
 
 
 class Form(StatesGroup):
     geo = State()
     answer = State()
+    nom1 = State()
+    nom2 = State()
+    nom3 = State()
     log = State()
 
 
-@dp.callback_query_handler(text="first", state=Form.answer)
-async def vote1(call: types.CallbackQuery):
-    result[0] += 1
-    await call.answer(text="Спасибо за участие в нашем мероприятии! Ждем всех 29 апреля на Campus Fest!",
-                      show_alert=True)
-    await Form.next()
-
-
-@dp.callback_query_handler(text="second", state=Form.answer)
-async def vote2(call: types.CallbackQuery):
-    result[1] += 1
-    await call.answer(text="Спасибо за участие в нашем мероприятии! Ждем всех 29 апреля на Campus Fest!",
-                      show_alert=True)
-    await Form.next()
-
-
-@dp.callback_query_handler(text="third", state=Form.answer)
-async def vote3(call: types.CallbackQuery):
-    result[2] += 1
-    await call.answer(text="Спасибо за участие в нашем мероприятии! Ждем всех 29 апреля на Campus Fest!",
-                      show_alert=True)
-    await Form.next()
-
-
-@dp.message_handler(commands="admin", state='*')
-async def admin_start(message: types.Message):
-    return await Form.geo.set()
-
-
-@dp.message_handler(lambda message: 'Baltic Eagles' in message.text, state=Form.geo)
-async def admin_check(message: types.Message):
-    global org_loc
-    if "1" in message.text:
-        org_loc = (org_loc1, org_loc2)
-    elif "2" in message.text:
-        org_loc = (org_loc3, org_loc4)
-    elif "3" in message.text:
-        org_loc = (org_loc5, org_loc6)
-    return await Form.next()
-
-
-@dp.message_handler(lambda message: message.text == 'Check', state=Form.answer)
-async def admin_res(message: types.Message, state: FSMContext):
-    await state.finish()
-    return await message.answer(", ".join(str(x) for x in result))
-
-
-@dp.message_handler(lambda message: message.text == 'Finish', state=Form.answer)
-async def admin_finish(message: types.Message, state: FSMContext):
-    await state.finish()
-    names = ['ZeЧипсы', 'Совиная голова', 'Прятки']
-    ans = dict()
-    for i in range(len(names)):
-        ans[names[i]] = result[i]
-    ans = dict(sorted(ans.items(), key=lambda item: item[1], reverse=True))
-    return await message.answer(str(ans))
-
-
-@dp.message_handler(commands="start")
-async def start(message: types.Message):
-    await Form.geo.set()
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton(text="Поделиться геолокацией", request_location=True))
+@form_router.message(CommandStart())
+async def start(message: types.Message, state: FSMContext):
+    await state.set_state(Form.geo)
+    keyboard = types.ReplyKeyboardMarkup(
+            keyboard=[
+                [types.KeyboardButton(text="Поделиться геолокацией",
+                                      request_location=True)]
+                ], 
+            resize_keyboard=True)
     await message.answer(
         "Добрый вечер! Здесь вы сможете оставить свой голос за самого понравившегося артиста! Вам необходимо поделиться геолокацией, чтобы мы могли проверить, что вы находитесь сейчас с нами в клубе",
         reply_markup=keyboard)
 
 
-@dp.message_handler(content_types=['location'], state=Form.geo)
-async def geo_start(msg: types.Message):
+@form_router.message(Form.geo)
+async def geo_start(msg: types.Message, state: FSMContext):
     print(msg.location)
     lat = msg.location.latitude
     lon = msg.location.longitude
     people_loc = (lat, lon)
     if geo.geodesic(org_loc, people_loc).m < 300:
-        await Form.next()
-        await msg.reply("Вы допущены к голосованию!",
+        await state.set_state(Form.nom1)
+        await msg.answer("Вы допущены к голосованию! В голосовании будет 3 номинации: лучший вокал, лучший инструментал, лучшее выступление. Выбирай внимаетльно",
                         reply_markup=types.ReplyKeyboardRemove())
-        await voting(msg)
-        print(result)
+        keyboard = types.ReplyKeyboardMarkup(
+                keyboard=[
+                    [types.KeyboardButton(text=band) for band in bands]
+                ],
+                resize_keyboard=True
+                )
+        await msg.answer("Выбери лучший вокал",
+                        reply_markup=keyboard)
     else:
         return await msg.answer("Вы находитесь слишком далеко от клуба! Вы можете попробовать снова.")
 
 
-# @dp.message_handler(state='*', commands='cancel')
-# @dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
-# async def cancel_handler(message: types.Message, state: FSMContext):
-#     await state.finish()
-#     await message.reply('ОК, \cancel так \cancel. Можешь начать сначала написав /start!')
+@form_router.message(Form.nom1)
+async def nom1_answer(msg: types.Message, state: FSMContext):
+    if msg.text in bands:
+        result["Лучший вокал"][bands.index(msg.text)] += 1
+        await state.set_state(Form.nom2)
+        await msg.answer("Выбери лучший инструментал")
+    else:
+        await msg.reply("не понял")
 
-@dp.message_handler(state=Form.answer)
-async def voting(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(text="ZeЧипсы", callback_data="first")).add(
-        types.InlineKeyboardButton(text="Совиная голова", callback_data="second")).add(
-        types.InlineKeyboardButton(text="Прятки", callback_data="third"))
-    await message.answer("Оставьте ваш голос!", reply_markup=keyboard)
 
+@form_router.message(Form.nom2)
+async def nom1_answer(msg: types.Message, state: FSMContext):
+    if msg.text in bands:
+        result["Лучший инструментал"][bands.index(msg.text)] += 1
+        await state.set_state(Form.nom3)
+        await msg.answer("Выбери лучшее выступление")
+    else:
+        await msg.reply("не понял")
+
+
+@form_router.message(Form.nom3)
+async def nom1_answer(msg: types.Message, state: FSMContext):
+    if msg.text in bands:
+        result["Лучшее выступление"][bands.index(msg.text)] += 1
+        await state.set_state(Form.log)
+        await msg.answer("Спасибо за участие!")
+        print(result)
+    else:
+        await msg.reply("не понял")
+
+
+@form_router.message(Command("biba"))
+async def nom1_answer(msg: types.Message, state: FSMContext):
+    message = ""
+    for key in result:
+        message += f"{key}:\n"
+        for band_ind in range(len(bands)):
+            message += f"-{bands[band_ind]} {result[key][band_ind]} голосов\n"
+
+    await msg.answer(message)
+    
+
+async def main():
+    bot = Bot(token=config.TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(form_router)
+
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     # Запуск бота
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    # start_webhook(
+    #     dispatcher=dp,
+    #     webhook_path=WEBHOOK_PATH,
+    #     on_startup=on_startup,
+    #     on_shutdown=on_shutdown,
+    #     skip_updates=True,
+    #     host=WEBAPP_HOST,
+    #     port=WEBAPP_PORT,
+    # )
+    asyncio.run(main())
+    
